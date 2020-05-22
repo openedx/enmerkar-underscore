871886e2f87f452bd0ef08500353ee9f2a88188d
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-05-22 17:47:30 +0500

    BOM-1637 Using enmerkar instead of commit hast.

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -3,7 +3,7 @@ import pytest
 from babel._compat import BytesIO
 from babel.messages.extract import DEFAULT_KEYWORDS
 
-from enmerkar.extract import extract_django as extract
+from enmerkar.extract import extract_django
 
 
 class TestMixedExtract:
@@ -14,35 +14,35 @@ class TestMixedExtract:
 
     def test_parses_django(self):
         buf = BytesIO(b'{% trans "Bunny" %}')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == ([(1, None, u'Bunny', [])])
 
     def test_parses_blocktrans(self):
         buf = BytesIO(b'Ignored {% blocktrans %}{{ anton }}{% endblocktrans %}')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == [(1, None, u'%(anton)s', [])]
 
     def test_parses_underscore(self):
         buf = BytesIO(b'hello: <%= name %>')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == []
 
     def test_parses_underscore_gettext(self):
         buf = BytesIO(b'hello: <%= gettext("name") %>')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == [(1, 'gettext', u'name', [])]
 
     def test_extract_unicode(self):
         buf = BytesIO(u'<%= gettext("@ſðæ314“ſſ¶ÐĐÞ→SÆ^ĸŁ") %>'.encode('utf-8'))
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == [(1, 'gettext', u'@ſðæ314“ſſ¶ÐĐÞ→SÆ^ĸŁ', [])]
 
     def test_extract_singular_form(self):
         buf = BytesIO(b'<%= _("foo") %> <%= _() %> <%= ngettext("foo", "bar", 42) %>')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == [(1, '_', u'foo', []), (1, '_', (), []), (1, 'ngettext', (u'foo', u'bar', None), [])]  # noqa
 
     def test_extract_singular_form_kwargs(self):
         buf = BytesIO(b'<%= _("foo") %> <%= _() %> <%= ngettext("foo", "bar", count=42) %>')
-        messages = list(extract(buf, self.keywords, [], {}))
+        messages = list(extract_django(buf, self.keywords, [], {}))
         assert messages == [(1, '_', u'foo', []), (1, '_', (), []), (1, 'ngettext', (u'foo', u'bar', None, None), [])]  # noqa
