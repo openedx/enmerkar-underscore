95ebade7be2158bd38b9637694f8c223fbae5ef0
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-06-29 19:20:09 +0500

    revert braces

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     os.system('python setup.py bdist_wheel upload')
     print('You probably want to also tag the version now:')
-    print(('  git tag -a %s -m "version %s"' % (version, version)))
+    print('  git tag -a %s -m "version %s"' % (version, version))
     print('  git push --tags')
     sys.exit()
 
